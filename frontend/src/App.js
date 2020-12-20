8078c0c089dbbd8635b42b35333a3f102c636cbd
alexxxstep <astep2004@gmail.com>
2020-12-21 00:35:33 +0200

    add server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import {} from 'react-router-bootstrap'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
-      <h1>Welcome to E-COMMERCE</h1>
-    </>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path="/" exact component={HomeScreen} />
+          <Route path="/products/:id" exact component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   )
 }
 
