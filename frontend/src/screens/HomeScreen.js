49217e507bbef447ea4e30d4543ef3ac957207b2
alexxxstep <astep2004@gmail.com>
2021-06-27 18:15:28 +0300

    correct smth

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,20 +1,23 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Row, Col } from 'react-bootstrap'
-import Product from '../components/Product'
-import { listProducts } from '../actions/productActions'
-import Loader from '../components/Loader'
-import Message from '../components/Message'
+import React, { useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {Row, Col} from 'react-bootstrap';
+import Product from '../components/Product';
+import {listProducts} from '../actions/productActions';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 
 const HomeScreen = () => {
-  const dispatch = useDispatch()
 
-  const productList = useSelector((state) => state.productList)
-  const { loading, error, products } = productList
+
+
+  const dispatch = useDispatch();
+
+  const productList = useSelector((state) => state.productList);
+  const {loading, error, products} = productList;
 
   useEffect(() => {
-    dispatch(listProducts())
-  }, [dispatch])
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <>
@@ -34,7 +37,7 @@ const HomeScreen = () => {
         </Row>
       )}
     </>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
